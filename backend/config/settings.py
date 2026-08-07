c29aad7e977041146cc1c8dece6b8a1fc4200c95
Amisha <amishag0000@gmail.com>
2026-08-07 17:53:17 +0530

    update config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,10 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
-    MISTRAL_MODEL = os.getenv("MISTRAL_MODEL")
-    EMAIL_ID = os.getenv("EMAIL_ID")
-    MONGO_URL = os.getenv("MONGO_URL")
-    EMAIL_PASS = os.getenv("EMAIL_PASS")
-    API_ROOT = os.getenv("API_ROOT")
-    FERNET_KEY = os.getenv("FERNET_KEY")
+    MISTRAL_API_KEY = os.environ["MISTRAL_API_KEY"]
+    MISTRAL_MODEL = os.environ["MISTRAL_MODEL"]
+
+    EMAIL_ID = os.environ["EMAIL_ID"]
+    EMAIL_PASS = os.environ["EMAIL_PASS"]
+
+    MONGO_URL = os.environ["MONGO_URL"]
+
+    API_ROOT = os.environ["API_ROOT"]
+
+    FERNET_KEY = os.environ["FERNET_KEY"]
